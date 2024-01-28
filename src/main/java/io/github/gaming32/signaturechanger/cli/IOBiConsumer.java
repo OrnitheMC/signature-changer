80076a317b2cd643e4b088b887d32b16a3c58ed9
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-01-28 11:25:39 -0600

    FunctionalInterface

--- src/main/java/io/github/gaming32/signaturechanger/cli/IOBiConsumer.java
+++ src/main/java/io/github/gaming32/signaturechanger/cli/IOBiConsumer.java
@@ -2,6 +2,7 @@ package io.github.gaming32.signaturechanger.cli;
 
 import java.io.IOException;
 
+@FunctionalInterface
 public interface IOBiConsumer<T, U> {
     void accept(T t, U u) throws IOException;
 }
--- src/main/java/io/github/gaming32/signaturechanger/cli/IOConsumer.java
+++ src/main/java/io/github/gaming32/signaturechanger/cli/IOConsumer.java
@@ -2,6 +2,7 @@ package io.github.gaming32.signaturechanger.cli;
 
 import java.io.IOException;
 
+@FunctionalInterface
 public interface IOConsumer<T> {
     void accept(T t) throws IOException;
 }
